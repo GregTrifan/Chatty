9f28157fe5269040dc88a69242b05286961cf42c
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2021-02-11 14:41:01 +0200

    Added 404 route

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ const App = () => {
         <Template>
             <Switch>
                 <Route exact path='/' component={Home} />
+                <Route path='/404' component={Lost}/>
                 <Route component={Lost}/>
             </Switch>
         </Template>
